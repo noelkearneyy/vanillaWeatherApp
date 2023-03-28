f103ff261e282ac65c3a9360b47c318c6b187f1d
Noel Kearney <noelkearney01@outlook.com>
2023-03-28 13:02:10 +0100

    frontend

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const searchLocationInput = document.querySelector('.locationSearchInput');
 const searchLocationIcon = document.querySelector('#locationSearchIcon');
 
-const searchDetails = {}
+const APIKey = "171073ee66f087671dbe9dfd93e6be69";
+
+let searchDetails = {
+    latitude: '',
+    longitude: '',
+    city: '',
+}
+
+const searchWeatherLocation = () => {
+    // const url = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=${APIKey}`
+    axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=54.5198883&lon=-6.0480218&appid=171073ee66f087671dbe9dfd93e6be69`).then((response) => {
+       console.log(response.data);
+     })
+     .catch((error) => console.error(error));
+   }
 
 searchLocationInput.addEventListener('input', (event) => {
     const searchValue = event.target.value;
-    
+    searchWeatherLocation()
+
 });
 
 searchLocationIcon.addEventListener('click', () => {
@@ -22,9 +37,12 @@ const userLocation = (location) => {
     const latitude = location.coords.latitude;
     const longitude = location.coords.longitude;
 
-    console.log(latitude, longitude);
+    searchDetails = {
+        ...searchDetails, 
+        latitude: latitude,
+        longitude: longitude,
+    }
+
+    console.log(searchDetails);
 }
 
-const searchWeatherLocation = () => {
-
-}
